aa741087f12d79bceece2a8d2d584bb38629e7fb
Patrick Eigensatz <patrick.eigensatz@gmail.com>
2015-09-06 04:44:14 +0200

    Server: Shitty code works shitty

--- src/server.py
+++ src/server.py
@@ -2,20 +2,44 @@
 
 import tornado.ioloop
 import tornado.web
+import tornado.websocket
 
-numberOfClients = 0
+from tornado.options import define, options, parse_command_line
 
+define("port", default=8888, help="run on the given port", type=int)
 
-class UserInfo(tornado.web.RequestHandler):
-	def get(self):
-		self.write("Hello, " + self.get_argument("username"))
-		
+# we gonna store clients in dictionary..
+clients = dict()
 
-application = tornado.web.Application([
-    (r"/", UserInfo),
+class IndexHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self):
+        self.write("This is your response")
+        self.finish()
+
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    def open(self, *args):
+        self.id = self.get_argument("Id")
+        self.stream.set_nodelay(True)
+        clients[self.id] = {"id": self.id, "object": self}
+
+    def on_message(self, message):        
+        """
+        when we receive some message we want some message handler..
+        for this example i will just print message to console
+        """
+        print("Client %s received a message : %s" % (self.id, message))
+        
+    def on_close(self):
+        if self.id in clients:
+            del clients[self.id]
+
+app = tornado.web.Application([
+    (r'/', IndexHandler),
+    (r'/ws', WebSocketHandler),
 ])
 
-
-if __name__ == "__main__":
-    application.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+if __name__ == '__main__':
+    parse_command_line()
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
