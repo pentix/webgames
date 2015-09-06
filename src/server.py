02873ef0d0fbc0c1c8ca61a43c6a239a5f998ef9
Patrick Eigensatz <patrick.eigensatz@gmail.com>
2015-09-06 04:16:24 +0200

    Tornado Server

--- src/server.py
+++ src/server.py
@@ -1,43 +1,21 @@
 #!/usr/bin/env python3
 
-import socketserver
+import tornado.ioloop
+import tornado.web
 
 numberOfClients = 0
 
-class WebgameServer(socketserver.BaseRequestHandler):
-	def handle(self):
-		global numberOfClients
-		numberOfClients = numberOfClients + 1
+
+class UserInfo(tornado.web.RequestHandler):
+	def get(self):
+		self.write("Hello, " + self.get_argument("username"))
 		
-		try:
-			while True:
-				self.data = self.request.recv(128)
-				if self.data != b'':
-					print(self.data)
-					print('Number of Clients: ' + str(numberOfClients) + '!')
-				
-				self.request.sendall(b'Number of Clients: ' + bytes(str.encode(str(numberOfClients))) + b'!\n')
 
-		except BrokenPipeError:
-			pass
-			
-		numberOfClients = numberOfClients - 1
-
-class ThreadedServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-	pass
+application = tornado.web.Application([
+    (r"/", UserInfo),
+])
 
 
 if __name__ == "__main__":
-	try:
-		HOST, PORT = "", 5051
-		
-		serv = ThreadedServer((HOST, PORT), WebgameServer)
-		serv.allow_reuse_address = True
-		serv.serve_forever()
-	
-	except KeyboardInterrupt:
-		print("\nShutting down server")
-		
-		serv.shutdown()
-		serv.server_close()
-
+    application.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
